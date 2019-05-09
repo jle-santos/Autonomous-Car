d5bb772f23790aa6ab8a8e6a11ea0182282e974b
jle-santos <49331367+jle-santos@users.noreply.github.com>
2019-05-09 09:12:53 -0700

    Sensor working

--- CSensor.cpp
+++ CSensor.cpp
@@ -22,6 +22,37 @@ int CSensor::open(std::string comm)
 
 void CSensor::getDistance()
 {
+	double diffTick;
+	uint32_t startTick, endTick, now, maxTime = 3000;
+	
+	//send a pulse and get the distance
+	gpioWrite(TRIGGER, HIGH);
+	gpioSleep(PI_TIME_RELATIVE, 0, 10);
+	gpioWrite(TRIGGER, LOW);
+	
+	now = gpioTick();
+	//std::cout << "Sending pulse\n";
+	while(gpioRead(ECHO) == LOW && (gpioTick() - now) < maxTime)
+	{
+	gpioSleep(PI_TIME_RELATIVE, 0, 100);
+	}
+	startTick = gpioTick();
+	//std::cout << "Waiting pulse\n";
+	while(gpioRead(ECHO) == HIGH && (gpioTick() - startTick < maxTime))
+	{
+		gpioSleep(PI_TIME_RELATIVE, 0, 100);
+	}
+	endTick = gpioTick();
+	
+	//std::cout << "Start: " << startTick/1000000 << "s | End time: " << endTick/1000000 << "\n";
+	diffTick = endTick - startTick;
+	//std::cout << "Elapsed time: " << diffTick << " us\n";
+	//for(int j = 1; j < MOVING_AVE; j++)
+	//	_distances[j] = _distances[j-1];
+		
+	_distances[0] = ((diffTick/1000000) * 34300)/2;
+	//std::cout << "Elapsed time: " << diffTick/1000000 << " us | D: " << _distance << " cm\n";
+	/*
 	struct timeval start;
 	struct timeval timeOut;
     struct timeval end;
@@ -100,12 +131,12 @@ void CSensor::enable()
 void CSensor::retrieveDistance(double &dist)
 {
 	double average = 0;
-	
+	/*
 	for(int i = 0; i < MOVING_AVE; i++)
 	{
 		average += (_distances[i]);
-	}
-	dist = average/5;
+	}*/
+	dist = _distances[0];
 }
 
 CSensor::~CSensor()
